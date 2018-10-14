cf84e4953ee716a2daea648115e3247bcdbc3d78
alexanderkostylev <alexander.rels01@yandex.com>
2018-10-14 23:33:04 +0500

    add scroll ))

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, ViewPagerAndroid, View, Text} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +8,30 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ViewPagerAndroid
+        style={styles.viewPager}
+        initialPage={0}>
+        <View style={{...styles.pageStyle, backgroundColor: 'powderblue'}} key="1">
+          <Text>First page</Text>
+        </View>
+        <View style={styles.pageStyle} key="2">
+          <Text>Second page</Text>
+        </View>
+      </ViewPagerAndroid>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  viewPager: {
+    flex: 1
+  },
+  pageStyle: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    padding: 20,
+  }
 });
